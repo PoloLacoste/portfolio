0acd5f4943373ded0837dec3cbff176af9fbce38
JM REMEUR <jm@remeur.fr>
2021-03-28 00:40:48 +0100

    update social buttons style

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-screen h-screen flex flex-col justify-center items-center bg-gray-800">
     <TagCloud class="h-full flex items-center" />
-    <div class="flex flex-col mb-2">
+    <div class="flex flex-col mb-2 items-center">
       <h1 class="text-white text-md md:text-xl">Jean-Michel REMEUR</h1>
       <SocialButtons class="mt-4" />
     </div>
--- app/src/components/SocialButtons.vue
+++ app/src/components/SocialButtons.vue
@@ -1,24 +1,84 @@
 <template lang="en">
     <div class="flex justify-center items-center">
-        <a href="https://github.com/PoloLacoste" class="social-button mr-4">
-            <img class="w-10 md:w-16" src="@/assets/github.png">
+        <a href="https://github.com/PoloLacoste" class="social-button github mr-4">
+            <font-awesome-icon class="fa" :icon="['fab', 'github']" />
         </a>
 
-        <a href="https://www.linkedin.com/in/jean-michel-remeur-471656148/" class="social-button">
-            <img class="w-10 md:w-16" src="@/assets/linkedin.png">
+        <a href="https://www.linkedin.com/in/jean-michel-remeur-471656148/" class="social-button linkedin">
+            <font-awesome-icon class="fa" :icon="['fab', 'linkedin']" />
         </a>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'SocialButtons'
-}
+  name: "SocialButtons",
+};
 </script>
 
 <style scoped>
 .social-button {
-    @apply transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110;
-    @apply text-white text-xl rounded-lg p-2;
+  display: inline-block;
+  width: 90px;
+  height: 90px;
+  line-height: 90px;
+  margin: 0 10px;
+  text-align: center;
+  position: relative;
+  overflow: hidden;
+  opacity: 0.99;
+  border-radius: 30%;
+  box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.05);
+  -webkit-transition: all 0.35s cubic-bezier(0.31, -0.105, 0.43, 1.59);
+  transition: all 0.35s cubic-bezier(0.31, -0.105, 0.43, 1.59);
+}
+
+.social-button:before {
+  content: "";
+  background-color: #000;
+  width: 120%;
+  height: 120%;
+  position: absolute;
+  top: 90%;
+  left: -110%;
+  -webkit-transform: rotate(45deg);
+  transform: rotate(45deg);
+  -webkit-transition: all 0.35s cubic-bezier(0.31, -0.105, 0.43, 1.59);
+  transition: all 0.35s cubic-bezier(0.31, -0.105, 0.43, 1.59);
+}
+
+.social-button .fa {
+    @apply text-5xl;
+  vertical-align: middle;
+  -webkit-transform: scale(0.8);
+  transform: scale(0.8);
+  -webkit-transition: all 0.35s cubic-bezier(0.31, -0.105, 0.43, 1.59);
+  transition: all 0.35s cubic-bezier(0.31, -0.105, 0.43, 1.59);
+}
+
+.social-button.github:before {
+  background-color: #000000;
+}
+.social-button.github .fa {
+  color: #000000;
+}
+
+.social-button.linkedin:before {
+  background-color: #007bb5;
+}
+.social-button.linkedin .fa {
+  color: #007bb5;
+}
+
+.social-button:focus:before,
+.social-button:hover:before {
+  top: -10%;
+  left: -10%;
+}
+.social-button:focus .fa,
+.social-button:hover .fa {
+  color: #fff;
+  -webkit-transform: scale(1);
+  transform: scale(1);
 }
 </style>
